d67e5e6f825b176712bd9a1cf8b6ccb3277ac40b
Supanat <k.suphanuch23@gmail.com>
2025-03-01 20:14:15 +0700

    lap24_1 complete

--- lab24_1.cpp
+++ lab24_1.cpp
@@ -63,4 +63,28 @@ void List::append(int d){
 	size++;
 }
 
-//Write List::remove() here
+void List::remove(int idx) {
+    if (root == NULL || idx < 0 || idx >= size) {
+        cout << "Invalid index.\n";
+        return;
+    }
+
+    Node* current = root;
+    
+    if (idx == 0) {
+        root = root->next;
+        delete current;
+        size--;
+        return;
+    }
+
+    Node* prev = NULL;
+    for (int i = 0; i < idx; i++) {
+        prev = current;
+        current = current->next;
+    }
+
+    prev->next = current->next;
+    delete current;
+    size--;
+}
